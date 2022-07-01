d26655a699b0cfcad59ac4886f026f0fef6d2e88
Loïc <61285174+LoicMuller@users.noreply.github.com>
2022-07-01 15:01:51 +0200

    message form working (wip)

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,20 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Entity\Status;
+use App\Form\MessageFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/message", name="message_")
  */
 class MessageController extends AbstractController
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/new", name="new")
      */
-    public function newMessage()
+    public function newMessage(Request $req, EntityManagerInterface $em): Response
     {
-        return $this->render('message/new.html.twig');
+        $message = new Message();
+        $form = $this->createForm(MessageFormType::class, $message);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->security->getUser();
+
+            $message->setUserId($user);
+            ($message->getPlanifiedAt() != null) ? $message->setStatus('planified') : $message->setStatus('sent');
+            $message->setCreatedAt(new \DateTimeImmutable('now'));
+
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('message/new.html.twig', [
+            'messageForm' => $form->createView(),
+        ]);
     }
 
     /**
---
+++ src/Form/MessageFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextType::class, [
+                'label' => 'Texte',
+                'required' => true,
+            ])
+            ->add('planified_at', DateTimeType::class, [
+                'placeholder' => [
+                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+                    'hour' => 'Hour', 'minute' => 'Minute',
+                ],
+                'required' => false,
+            ])
+            ->add('socialMedia', ChoiceType::class, [
+                'label' => 'Reseau social',
+                'choices' => [
+                    'Slack' => 'slack',
+                    'Discord' => 'discord',
+                ],
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
